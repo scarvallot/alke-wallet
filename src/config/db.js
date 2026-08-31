4a7501411d698317a293488903a9ad8fd12dd4d9
se.carvallo <s.carvallot@gmail.com>
2026-08-30 22:39:39 -0400

    feat: ajustar manejo de conexiones en la base de datos y mejorar la liberación de recursos

--- src/config/db.js
+++ src/config/db.js
@@ -5,22 +5,23 @@ require("dotenv").config();
 const pool = mysql.createPool({
   uri: process.env.DATABASE_URL,
   connectionLimit: 10,
-  connectTimeout: 30000, // Tiempo de espera para establecer la conexión en milisegundos
-  idleTimeout: 30000, // Tiempo de espera para liberar conexiones inactivas en milisegundos
   waitForConnections: true,
 });
 
 // Comprueba que la conexión inicial a la base de datos funcione correctamente.
 const testConnection = async () => {
+  let connection;
   try {
-    const connection = await pool.getConnection();
+    connection = await pool.getConnection();
     console.log("Conexión exitosa a la base de datos MySQL");
     connection.release();
   } catch (error) {
     console.error("Error al conectar con la base de datos.", error.message);
   } finally {
-    await pool.end(); // Cierra el pool de conexiones al finalizar la prueba
-    console.log("Pool de conexiones cerrado.");
+    if (connection) connection.release(); // Asegura que la conexión se libere en caso de error
+    console.log("Conexión liberada al poil.");
+    // await pool.end(); // Cierra el pool de conexiones al finalizar la prueba
+    // console.log("Pool de conexiones cerrado.");
   }
 };
 
