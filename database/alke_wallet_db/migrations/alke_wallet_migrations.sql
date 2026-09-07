035595db75a7c2a4f2bec1b0578beaaf0cee02f2
se.carvallo <s.carvallot@gmail.com>
2026-09-07 20:23:29 -0300

    refactor(database): actualizar la estructura de la tabla de usuarios

--- database/alke_wallet_db/migrations/alke_wallet_migrations.sql
+++ database/alke_wallet_db/migrations/alke_wallet_migrations.sql
@@ -1,4 +1,4 @@
--- =====================================================
+SELECT * FROM alkewallet.users;-- =====================================================
 -- Migraciones — alke_wallet_db
 -- Vacío: CHECK y restricciones están en schema/01_alke_wallet_schema.sql
 -- =====================================================
@@ -12,9 +12,12 @@ ALTER TABLE alkewallet.users ADD COLUMN is_active TINYINT(1) NOT NULL DEFAULT 1;
 ALTER TABLE alkewallet.users MODIFY COLUMN created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP; /* Se modifica la columna created_at de la tabla users para establecer un valor predeterminado de CURRENT_TIMESTAMP, lo que significa que se registrará automáticamente la fecha y hora de creación del registro. */
 ALTER TABLE alkewallet.users ADD COLUMN updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;    /* Se agrega la columna updated_at a la tabla users para almacenar la fecha y hora de la última actualización del registro. Se establece un valor predeterminado de CURRENT_TIMESTAMP y se actualiza automáticamente cada vez que se modifica el registro. */
 
-/*  Modificación de la tabla users para cambiar el orden de las columnas y establecer restricciones NOT NULL en first_name y last_name.
-ALTER TABLE users MODIFY COLUMN created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER is_active;
+#	Modificación de la tabla users para cambiar el orden de las columnas y establecer restricciones NOT NULL en first_name y last_name.
 ALTER TABLE alkewallet.users MODIFY COLUMN first_name VARCHAR(255) NOT NULL AFTER user_name;
 ALTER TABLE alkewallet.users MODIFY COLUMN last_name VARCHAR(255) NOT NULL AFTER first_name;
-*/
+ALTER TABLE users MODIFY COLUMN created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP AFTER is_active;
 
+
+
+INSERT INTO alkewallet.users (user_name, first_name, last_name, email, password, is_active) 
+VALUES('admin', 'system', 'Administrator', 'admin@alkewallet.com',12345,1); /* Se inserta un registro en la tabla users con los valores especificados para user_name, first_name, last_name, email, password e is_active. Esto crea un usuario administrador con los datos proporcionados. */
